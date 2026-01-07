f2b11dc38a295b41a60b18a0d04283abe4edb439
Spontaniczny <k2002kuba@gmail.com>
2026-01-08 00:34:04 +0100

    Created main now because instructions unclear

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.scripts.download_artifacts import download_artifacts
+from src.scripts.export_sentence_transformer_to_onnx import export_model_to_onnx
+from src.scripts.settings import Settings
+from src.scripts.export_classifier_to_onnx import export_classifier_to_onnx
+
+
+
+
+if __name__ == "__main__":
+    settings = Settings()
+
+    download_artifacts(settings)
+    export_model_to_onnx(settings)
+    export_classifier_to_onnx(settings)
